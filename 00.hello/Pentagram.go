4a1edf19cff9af31a9f34c1d6caa7fa58e15f38e
xiongyi0105 <1019487236@qq.com>
2021-11-25 22:22:48 +0800

    00.hello and Pentagram

---
+++ 00.hello/Pentagram.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func Pentagram() string {
+	fmt.Println("         *  ")
+	fmt.Println("        * *  ")
+	fmt.Println("       * * * ")
+	fmt.Println(" * * * * * * * * * *  ")
+	fmt.Println("    * * * * * * * ")
+	fmt.Println("     * * * * * ")
+	fmt.Println("    * *     * * ")
+	fmt.Println("    *        * ")
+	return ""
+}
---
+++ 00.hello/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("你好, golang! ")
+	Pentagram()
+
+}
